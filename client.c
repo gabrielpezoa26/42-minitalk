f44790f2087f47bd15744c699a0e3a9193ef665a
Gabriel <gabrielpezoa26@gmail.com>
2025-01-26 20:31:23 -0300

    d

--- client.c
+++ client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 18:52:45 by gabriel           #+#    #+#             */
-/*   Updated: 2025/01/26 13:57:29 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/01/26 19:04:15 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,25 @@ static void	ack_signal(int signal)
 static void	send_message(unsigned int server_pid, char *message, size_t len)
 {
 	int		bit_index;
-	size_t	i;
+	size_t	char_index;
 
-	i = 0;
-	while (i < len)
+	char_index = 0;
+	while (char_index < len)  //anda por cada char
 	{
 		bit_index = 0;
-		while (bit_index < 8)
+		while (bit_index < 8)  //processa cada char
 		{
-			if ((message[i] >> (7 - bit_index)) & 1)
-				kill(server_pid, SIGUSR1);
+			if ((message[char_index] >> (7 - bit_index)) & 1)  //move os bits do char atual 1 bit pra direita
+				kill(server_pid, SIGUSR1);  //manda '1'
 			else
-				kill(server_pid, SIGUSR2);
+				kill(server_pid, SIGUSR2);  //manda '0'
 			bit_index++;
-			usleep(1000);
-			while (!g_flag)
+			usleep(1000);  //espera o sinal d volta do server
+			while (g_flag == 0)
 				pause();
-			g_flag = 0;
+			g_flag = 0;  //reseta pro prox bit
 		}
-		i++;
+		char_index++;
 	}
 }
 
@@ -49,18 +49,18 @@ int	main(int argc, char **argv)
 {
 	unsigned int	server_pid;
 
-	if (argc != 3)
+	if (argc != 3)  //tratativa d erro
 	{
 		ft_printf("Format: ./client <server_pid> <message>\n");
 		return (1);
 	}
-	server_pid = ft_atoi(argv[1]);
-	if (server_pid <= 0)
+	server_pid = ft_atoi(argv[1]);  //converte pid
+	if (server_pid <= 0)  //tratativa d err
 	{
 		ft_printf("Invalid PID.\n");
 		return (1);
 	}
-	signal(SIGUSR1, ack_signal);
+	signal(SIGUSR1, ack_signal);  //define func responsavel sinal recebido
 	send_message(server_pid, argv[2], ft_strlen(argv[2]) + 1);
 	return (0);
 }
--- server.c
+++ server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 18:52:52 by gabriel           #+#    #+#             */
-/*   Updated: 2025/01/26 12:40:48 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/01/26 18:09:57 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,61 +21,61 @@ static void	ft_assemble_msg(void)
 
 	temp = NULL;
 	c = (char)g_to_print.c;
-	if (g_to_print.msg)
+	if (g_to_print.msg != NULL)  //se ja tiver algo, concatena e libera a memória
 	{
-		temp = ft_strjoin(g_to_print.msg, &c);
+		temp = ft_strjoin(g_to_print.msg, &c);  //concatena a msg a partir de '&c'
 		free(g_to_print.msg);
 	}
 	else
-		temp = ft_strdup(&c);
-	if (!temp)
+		temp = ft_strdup(&c);  //cria nova string, so oq tem em "c"
+	if (temp == NULL)
 		exit(EXIT_FAILURE);
-	g_to_print.msg = temp;
+	g_to_print.msg = temp; //aponta pra nova string
 }
 
 static void	ft_print_character(void)
 {
-	if (g_to_print.c == '\0')
+	if (g_to_print.c == '\0')  //é pq chegou no final da string
 	{
-		if (g_to_print.msg)
-			ft_printf("%s\n", g_to_print.msg);
+		if (g_to_print.msg != NULL)
+			ft_printf("%s\n", g_to_print.msg);  //printa os bagui, %s
 		else
-			ft_printf("\n");
+			ft_printf("\n"); 
 		free(g_to_print.msg);
-		g_to_print.msg = NULL;
+		g_to_print.msg = NULL;  //reseta o ponteiro
 	}
 	else
-		ft_assemble_msg();
-	g_to_print.c = 0;
+		ft_assemble_msg();  //se n tiver "completa"
+	g_to_print.c = 0;  //reseta e prepara pro prox char
 }
 
 static void	ft_handle_signal(int signal, siginfo_t *info, void *context)
 {
 	static int	bit_count = 0;
 
-	(void)context;
-	if (signal == SIGUSR1)
-		g_to_print.c = (g_to_print.c << 1) | 1;
+	(void)context;  //prototipo
+	if (signal == SIGUSR1) 
+		g_to_print.c = (g_to_print.c << 1) | 1;  //muda os bit uma posic pra esquerda, dando espaço pro prox
 	else if (signal == SIGUSR2)
-		g_to_print.c = (g_to_print.c << 1);
+		g_to_print.c = (g_to_print.c << 1);  // n precisa do "| 1" pq já é zero
 	bit_count++;
-	if (bit_count == 8)
+	if (bit_count == 8)  //qdo acumula 8 bits é pq formou um char
 	{
 		ft_print_character();
-		bit_count = 0;
+		bit_count = 0;  //reseta
 	}
-	if (kill(info->si_pid, SIGUSR1) == -1)
-		exit(EXIT_FAILURE);
+	if (kill(info->si_pid, SIGUSR1) == -1)  //"avisa" que recebeu o bit
+		exit(EXIT_FAILURE);  //se a func "kill" der ruim
 }
 
 int	main(void)
 {
 	struct sigaction	sa;
 
-	ft_printf("Server PID: %d\n", getpid());
-	sa.sa_sigaction = ft_handle_signal;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
+	ft_printf("Server PID: %d\n", getpid());  //printa o PID
+	sa.sa_sigaction = ft_handle_signal;  //fala qual func trata dos sinais
+	sa.sa_flags = SA_SIGINFO;  // permite o uso do struct "siginfo" pela func de signal_handle
+	sigemptyset(&sa.sa_mask);  //define q n vai bloquear nenhum sinal
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
