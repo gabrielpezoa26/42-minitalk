978aabb79d5b9d87b0bf3db580d619b53e67f8e6
gabriel <gabrielpezoa26@gmail.com>
2025-02-16 17:17:02 -0300

    .h

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 14:47:32 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/01/26 17:38:33 by gabriel          ###   ########.fr       */
+/*   Updated: 2025/02/16 17:16:48 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ typedef struct s_message
 {
 	unsigned char	c;
 	char			*msg;
-}t_message;
+}	t_message;
 
 #endif
